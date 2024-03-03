22287aedf642dd00915e23a1478321df0c9b4baa
Masudur400 <md8004210@gmail.com>
2024-03-03 16:56:47 +0600

    last update

--- script.js
+++ script.js
@@ -14,8 +14,7 @@
                         <div class="absolute">
                             <img class="w-[31%] mb-5 lg:w-[24%] rounded-lg" src="${item.image}" alt="">
                         </div>
-                        <div class="w-[10px] h-[10px] rounded-full bg-green-500  relative left-20  lg:top-0 lg:left-36">
-
+                        <div class="w-[10px] h-[10px] rounded-full bg-green-500  relative left-20  lg:top-0 lg:left-36"> 
                         </div>
                     </div>
                     <div class="lg:w-full">
@@ -30,15 +29,14 @@
                             <p><i class="fa-regular fa-message"></i> <span>${item.comment_count}</span></p>
                             <p><i class="fa-regular fa-eye"></i><span> ${item.view_count}</span></p>
                             <p><i class="fa-regular fa-clock"></i> <span> ${item.posted_time}</span> min</p>
-                            <div>
-                                <button class="h-[30px] w-[30px] rounded-full bg-green-500 "><i class="fa-regular fa-envelope-open text-le font-bold text-white"></i></button>
+                            <div class="text-center">
+                                <button onclick="clickHandle('${item.title}','${item.view_count}')" class=" btn  rounded-full bg-green-500 "><i class="fa-regular fa-envelope-open text-2xl font-bold text-white"></i></button>
                             </div>
                         </div>
                     </div>
                 </div>  
         `;
-        cardContainer.appendChild(div);
-        
+        cardContainer.appendChild(div); 
      })
  }
 
@@ -49,8 +47,9 @@
     const latestData= data;
     const latestPostContainer = document.getElementById('latestPost-container');
     latestPostContainer.textContent = '';
+
     latestData.forEach((item)=>{
-        console.log(item.profile_image);
+        // console.log(item.profile_image);
         const div = document.createElement('div');
         div.innerHTML = `
         <div class="bg-gray-50 border-2 border-gray-200 rounded-xl p-4">
@@ -76,6 +75,26 @@
         latestPostContainer.appendChild(div);
 
     })
+
+ }
+
+let count = 1 ;
+ const clickHandle =(title,view)=>{
+console.log(title,view);
+ 
+     const mark = document.getElementById('mark');
+     mark.innerText = count;
+     count = count +1 ;
+
+     const clickShowCard = document.getElementById('click-show-card');
+     const div = document.createElement('div');
+     div.innerHTML = `
+     <div class="lg:flex gap-10 mb-6 bg-white rounded-lg p-4">
+        <h3 class="text-base lg:w-[80%] lg:text-lg font-bold">${title}</h3>
+        <p class="flex mt-5 lg:w-[20%]"><i class="fa-regular fa-eye">  <span>${view}</span></i></p>
+    </div>
+     `;
+     clickShowCard.appendChild(div);
 
  }
 
